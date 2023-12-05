1e0c72b948c0d752f9276294d01c200996ae1ec4
paulb <paulb@ashridgetech.com>
2023-12-05 12:46:50 +0000

    Set maxInstances in extension.yaml

--- functions/test/accessToken.ts
+++ functions/test/accessToken.ts
@@ -3,7 +3,7 @@ import fs from 'fs'
 import {getAccessToken} from './testUtil'
 
 const getToken = async () => {
-    const serviceAccountKey = JSON.parse(fs.readFileSync('private/stone-frog-world-firebase-adminsdk-ac9t4-e864115f83.json', 'utf8'))
+    const serviceAccountKey = JSON.parse(fs.readFileSync('private/elemento-hosting-test-firebase-adminsdk-7en27-5ef2e44a5b-2.json', 'utf8'))
     const firebaseAccessToken = await getAccessToken(serviceAccountKey)
     console.log(firebaseAccessToken)
 }
